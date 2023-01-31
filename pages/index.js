1fc25eef2fb2adfdfbaa50b0cdfcb5ddd1910cb0
Faraz Ahmed <muhmdfaraz@gmail.com>
2023-01-31 15:38:11 +0500

    create product cards

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/layout';
+import ProductItem from '@/components/ProductItem';
+import data from '../utilites/data';
 
 export default function Home() {
+  console.log(data);
   return (
     <>
-      <Layout title={'Home Page'}>home page</Layout>
+      <Layout title={'Home Page'}>
+        <div className="grid gird-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => {
+            return (
+              <ProductItem product={product} key={product.slug}></ProductItem>
+            );
+          })}
+        </div>
+      </Layout>
     </>
   );
 }
